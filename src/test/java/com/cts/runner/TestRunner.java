52743748629b217ad16d0f24cc1b2c2eb1ff68fb
HARIKA <mohithayarramsetty@gmail.com>
2020-03-24 22:43:34 +0530

    first commit

--- src/test/java/com/cts/runner/TestRunner.java
+++ src/test/java/com/cts/runner/TestRunner.java
@@ -8,7 +8,6 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features = {"src/main/resources/features/DemoWebShop.feature"},
 glue = {"com/cts/stepDefinitions"}, plugin = {"html:reports/"})
 
-
 @RunWith(Cucumber.class)
 public class TestRunner {
 	
